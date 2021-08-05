3146620b8469c40759ef9beaefb529426f13c3de
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2021-08-05 19:52:59 +0200

    Buttons collecting class. (resources because of the renaming issues)

---
+++ FrostByte/Buttons.cs
@@ -0,0 +1,18 @@
+﻿namespace FrostByte
+{
+    class Buttons
+    {
+        public bool Enter, Space;
+        public bool Up, Down, Left, Right;
+        public bool W, S, A, D;
+        public int Pause;
+
+        public Buttons()
+        {
+            Enter = Space = false;
+            Up = Down = Left = Right = false;
+            W = A = S = D = false;
+            Pause = 0;
+        }
+    }
+}
